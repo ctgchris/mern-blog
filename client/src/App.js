f52359539d5c334e5971aed37c9b3b181dec15f4
ctgchris <ctgchris@github.com>
2024-01-03 19:23:28 -0500

    create react app and add basic layout

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
+            alt=""
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>UMD Research Development</h2>
+          <p>
+            The mission of the Research Development Office is to grow the volume
+            and visibility of the University of Maryland’s research enterprise.
+            
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
+            alt=""
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>UMD Research Development</h2>
+          <p>
+            The mission of the Research Development Office is to grow the volume
+            and visibility of the University of Maryland’s research enterprise.
+            
+          </p>
+        </div>
+      </div>
+      <div className="post">
+      < div className="image">
+          <img
+            src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
+            alt=""
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>UMD Research Development</h2>
+          <p>
+            The mission of the Research Development Office is to grow the volume
+            and visibility of the University of Maryland’s research enterprise.
+            
+          </p>
+        </div>
+      </div>
+      
+    </main>
+  );
+}
+
+export default App;
