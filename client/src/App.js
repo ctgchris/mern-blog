30fd4a5c3790911b350c4d46fb574d185697b628
ctgchris <ctgchris@github.com>
2024-01-03 19:52:02 -0500

    add routing for Header and Post

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,17 @@
 import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
 
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">
-          MyBlog
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+      <Header />
+      <Post />
+      <Post />
+      <Post />
 
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
-            alt=""
-          ></img>
-        </div>
-        <div className="texts">
-          <h2>UMD Research Development</h2>
-          <p className="info">
-            <a className="author">Chris Giang</a>
-            <time>2024-01-03 16:45</time>
-          </p>
-          <p className="summary">
-            The mission of the Research Development Office is to grow the volume
-            and visibility of the University of Maryland’s research enterprise.
-            
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
-            alt=""
-          ></img>
-        </div>
-        <div className="texts">
-          <h2>UMD Research Development</h2>
-          <p className="info">
-            <a className="author">Chris Giang</a>
-            <time>2024-01-03 16:45</time>
-          </p>
-          <p className="summary">
-            The mission of the Research Development Office is to grow the volume
-            and visibility of the University of Maryland’s research enterprise.
-            
-          </p>
-        </div>
-      </div>
-      <div className="post">
-      < div className="image">
-          <img
-            src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
-            alt=""
-          ></img>
-        </div>
-        <div className="texts">
-          <h2>UMD Research Development</h2>
-          <p className="info">
-            <a className="author">Chris Giang</a>
-            <time>2024-01-03 16:45</time>
-          </p>
-          <p className="summary">
-            The mission of the Research Development Office is to grow the volume
-            and visibility of the University of Maryland’s research enterprise.
-            
-          </p>
-        </div>
-      </div>
+      
+      
       
     </main>
   );
---
+++ client/src/Post.js
@@ -0,0 +1,23 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src="https://research.umd.edu/sites/default/files/styles/optimized/public/2021-12/umd-overhead-view-campus.jpg?itok=WKoGltIQ"
+          alt=""
+        ></img>
+      </div>
+      <div className="texts">
+        <h2>UMD Research Development</h2>
+        <p className="info">
+          <a className="author">Chris Giang</a>
+          <time>2024-01-03 16:45</time>
+        </p>
+        <p className="summary">
+          The mission of the Research Development Office is to grow the volume
+          and visibility of the University of Maryland’s research enterprise.
+        </p>
+      </div>
+    </div>
+  );
+}
